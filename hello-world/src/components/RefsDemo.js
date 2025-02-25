7ed340523bda7aa46aa1c3954d59856f4d8384cc
MarkHollander <minhvudoi3@gmail.com>
2025-02-25 16:07:26 +0700

    Reset commit

--- hello-world/src/components/RefsDemo.js
+++ hello-world/src/components/RefsDemo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-<<<<<<< HEAD
 export class RefsDemo extends Component {
     constructor(props) {
         super(props)
@@ -31,29 +30,9 @@ export class RefsDemo extends Component {
         <input type="text" ref={this.inputRef}/>
         <input type="text" ref={this.setCBRef}/>
         <button onClick={this.clickHandler}>Click</button>
-=======
-class RefsDemo extends Component {
-    constructor(props) {
-        super(props)
-        this.inputRef = React.createRef()
-    }
-
-    componentDidMount() {
-        this.inputRef.current.focus()
-        console.log(this.inputRef)
-    }
-  render() {
-    return (
-      <div>
-        <input type="text" ref = {this.inputRef} />
->>>>>>> ea72bbeabbcd3c10ee44a20bf023403dcce1de94
       </div>
     )
   }
 }
 
-<<<<<<< HEAD
 export default RefsDemo
-=======
-export default RefsDemo
->>>>>>> ea72bbeabbcd3c10ee44a20bf023403dcce1de94
